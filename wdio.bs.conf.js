ec6637ff11ddc01413c88c42e082e657a3c507c6
monthonkachana <maxmonthonkachana2@gmail.com>
2024-01-18 15:50:50 +0700

    edit ci V4

--- wdio.bs.conf.js
+++ wdio.bs.conf.js
@@ -13,7 +13,7 @@ exports.config = {
       },
     ],
   ],
-  specs: ["npx wdio run wdio.bs.conf.js --spec .\test\specs\login.mobile.js"],
+  // specs: ["npx wdio run wdio.bs.conf.js --spec .\test\specs\login.mobile.js"],
   capabilities: [
     {
       "bstack:options": {
