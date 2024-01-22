6906450f0652c0ade5da862a935a89ca620b5a8a
monthonkachana <maxmonthonkachana2@gmail.com>
2024-01-22 08:44:43 +0700

    swipe right and letf

--- test/basic/swipeHorizontal.js
+++ test/basic/swipeHorizontal.js
@@ -1,50 +1,107 @@
+// describe("Learning API", () => {
+//   it("should be Swipe Screen from right and left ", async () => {
+//     const Form_ICON_Swipe_SCREEN = "~Swipe";
+//     const FORM_HEADER = "~Swipe-screen";
+//     //setting swipe Horizontal from right and left
+//     const { height, width } = driver.getWindowSize();
+
+//     const anchorPercentage = 50;
+//     const startPointPercentage = 90;
+//     const endPointPercentage = 10;
+
+//     const anchor = (height * anchorPercentage) / 100;
+//     const startPoint = (width * startPointPercentage) / 100;
+//     const endPoint = (width * endPointPercentage) / 100;
+
+//     await $(Form_ICON_Swipe_SCREEN).click();
+//     await $(FORM_HEADER).click();
+
+//     for (let t = 0; t < 5; t++) {
+//       driver.touchPerform([
+//         {
+//           action: "press",
+//           options: {
+//             x: startPoint,
+//             y: anchor,
+//           },
+//         },
+//         {
+//           action: "wait",
+//           options: {
+//             ms: 1000,
+//           },
+//         },
+//         {
+//           action: "moveTo",
+//           options: {
+//             x: endPoint,
+//             y: anchor,
+//           },
+//         },
+//         {
+//           action: "release",
+//           options: {},
+//         },
+//       ]);
+//     }
+//     await browser.pause(3000);
+//   });
+// });
+
+
 describe("Learning API", () => {
-  it("should be Swipe Screen Vertically ", async () => {
-    const Form_ICON_HOME_SCREEN = "~Forms";
-    const BUTTON_ICON_ACTIVE = "~button-Active";
-    const FORM_HEADER = "~Forms-screen";
-    //setting swipe Horizontal
-    const { height } = driver.getWindowSize();
+  it("should be Swipe Screen from right and left ", async () => {
+    // Assuming you have initialized the WebDriver instance as 'driver'
+    // Replace 'driver' with your actual WebDriver instance
+
+    const Form_ICON_Swipe_SCREEN = "~Swipe";
+    const FORM_HEADER = "~Swipe-screen";
+
+    // Make sure to use the correct WebDriver method to get window size
+    const { height, width } = browser.getWindowSize();
 
     const anchorPercentage = 50;
     const startPointPercentage = 90;
     const endPointPercentage = 10;
 
     const anchor = (height * anchorPercentage) / 100;
-    const startPoint = (height * startPointPercentage) / 100;
-    const endPoint = (height * endPointPercentage) / 100;
+    const startPoint = (width * startPointPercentage) / 100;
+    const endPoint = (width * endPointPercentage) / 100;
 
-    await $(Form_ICON_HOME_SCREEN).click();
+    // Use appropriate WebDriver commands to interact with elements
+    await $(Form_ICON_Swipe_SCREEN).click();
     await $(FORM_HEADER).click();
 
-    driver.touchPerform([
-      {
-        action: "press",
-        options: {
-          x: anchor,
-          y: startPoint,
+    for (let t = 0; t < 5; t++) {
+      // Replace 'driver.touchPerform' with the correct method for your WebDriver
+      browser.touchPerform([
+        {
+          action: "press",
+          options: {
+            x: startPoint,
+            y: anchor,
+          },
         },
-      },
-      {
-        action: "wait",
-        options: {
-          ms: 1000,
+        {
+          action: "wait",
+          options: {
+            ms: 1000,
+          },
         },
-      },
-      {
-        action: "moveTo",
-        options: {
-          x: anchor,
-          y: endPoint,
+        {
+          action: "moveTo",
+          options: {
+            x: endPoint,
+            y: anchor,
+          },
         },
-      },
-      {
-        action: "release",
-        options: {},
-      },
-    ]);
+        {
+          action: "release",
+          options: {},
+        },
+      ]);
+    }
 
-    await $(BUTTON_ICON_ACTIVE).click();
     await browser.pause(3000);
   });
 });
